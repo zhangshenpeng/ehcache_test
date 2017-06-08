16cfe88167f0bcd0e5157013962ac80045982fa3
Cindy Chen <cindychen@CindydeMacBook-Air.local>
2017-06-08 08:38:11 +0800

    update code

--- src/test/java/com/inveno/ehcache_test/EhcacheTest.java
+++ src/test/java/com/inveno/ehcache_test/EhcacheTest.java
@@ -1,8 +1,9 @@
 package com.inveno.ehcache_test;
 import net.sf.ehcache.Cache;  
 import net.sf.ehcache.CacheManager;  
-import net.sf.ehcache.Element;  
-
+import net.sf.ehcache.Element; 
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.store.MemoryStoreEvictionPolicy;
 
 import static org.junit.Assert.assertTrue;
 
@@ -21,11 +22,12 @@ import java.util.Set;
 import org.junit.Test;
 
 public class EhcacheTest {
-    private static CacheManager cacheManager;  
-    static String               cacheName = "cache1";  
+ 
     
     @Test
-    public void test() {
+    public void testSimple() {
+        CacheManager cacheManager;  
+        String cacheName = "cache1"; 
         cacheManager = CacheManager.create(); 
         cacheManager.addCache(cacheName); 
         Cache cache = cacheManager.getCache(cacheName);  
@@ -36,6 +38,102 @@ public class EhcacheTest {
         
         Element e = cache.get(key);
         System.out.println(e);
-        
+        cacheManager.shutdown();
     }
+    
+    @Test
+    public void testWithConfiguration() {
+    	int maxElements = 10;
+    	Cache cache = new Cache(  
+    			  new CacheConfiguration("testCache", maxElements)  
+    			    .memoryStoreEvictionPolicy(MemoryStoreEvictionPolicy.LFU)  
+    			    .eternal(false)  
+    			    .timeToLiveSeconds(60)  
+    			    .timeToIdleSeconds(30)  
+    			    .diskExpiryThreadIntervalSeconds(0)); 
+        String key = "key1";  
+        String value = "value1"; 
+        
+        cache.put(new Element(key, value)); 
+        
+        Element e = cache.get(key);
+        System.out.println(e);
+    }
+    
+    @Test
+    public void testWithXml() {
+    	System.out.println(System.getProperty("java.io.tmpdir")); 
+    	//CacheManager cacheManager=CacheManager.getInstance(); // 从class path获取
+    	CacheManager manager = CacheManager.newInstance("src/test/resources/ehcache.xml"); 
+    	String cacheName = "userCache";
+    	String key = "key";
+    	String value = "value";
+    	Cache cache = manager.getCache(cacheName);  
+    	
+    	// set/get
+    	cache.put(new Element(key, value));  
+        Element e = cache.get(key);
+        System.out.println("value:" + e.getObjectValue());
+        
+        // expire
+		try {
+			Thread.sleep(5000);
+		} catch(Exception exception) {
+			System.out.println("sleep exception as " + exception);
+		}
+        e = cache.get(key);
+        System.out.println("value:" + e);
+		System.out.println("done");
+        //System.out.println("value list:" + cache.getKeys());
+        manager.shutdown();  
+    }
+    
+    @Test
+    public void testDisk() {
+    	System.out.println(System.getProperty("java.io.tmpdir")); 
+    	//CacheManager cacheManager=CacheManager.getInstance(); // 从class path获取
+    	CacheManager manager = CacheManager.newInstance("src/test/resources/ehcache_disk.xml"); 
+    	String cacheName = "userCache2";
+    	Cache cache = manager.getCache(cacheName);  
+   
+        
+        // lru
+        for(int i = 0; i < 1000; ++i) {
+        	String keyTmp = "key" + Integer.toString(i);
+        	String valueTmp = "value" + Integer.toString(i);
+        	cache.put(new Element(keyTmp,valueTmp));
+        	cache.flush();
+        }
+        // expire
+		try {
+			Thread.sleep(1500);
+		} catch(Exception exception) {
+			System.out.println("sleep exception as " + exception);
+		}
+		System.out.println("done");
+        //System.out.println("value list:" + cache.getKeys());
+        manager.shutdown();  
+    }
+    
+    @Test
+    public void testLru() {
+    	System.out.println(System.getProperty("java.io.tmpdir")); 
+    	//CacheManager cacheManager=CacheManager.getInstance(); // 从class path获取
+    	CacheManager manager = CacheManager.newInstance("src/test/resources/ehcache_lru.xml"); 
+    	String cacheName = "userCache3";
+    	Cache cache = manager.getCache(cacheName);  
+   
+        
+        // lru
+        for(int i = 0; i < 20; ++i) {
+        	String keyTmp = "key" + Integer.toString(i);
+        	String valueTmp = "value" + Integer.toString(i);
+        	cache.put(new Element(keyTmp,valueTmp));
+        }
+
+		System.out.println("done");
+        System.out.println("value list:" + cache.getKeys());
+        manager.shutdown();  
+    }
+    
 }
